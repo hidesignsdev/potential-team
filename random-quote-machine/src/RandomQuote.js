d4b65fcb17f1981e5620608ef62204ce873264bc
c2cdev006 <c2c.dev.006@gmail.com>
2020-03-12 15:59:40 +0700

    add twitter link
    add icon, link of twitter

--- random-quote-machine/src/RandomQuote.js
+++ random-quote-machine/src/RandomQuote.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "./styles.css";
-import {FaQuoteLeft} from "react-icons/fa";
+import { FaQuoteLeft, FaTwitter } from "react-icons/fa";
 
 const quoteDefault =
   "It’s your place in the world; it’s your life. Go on and do all you can with it, and make it the life you want to live.";
 const authorDefault = "Oprah Winfrey";
-  class RandomQuote extends Component {
+class RandomQuote extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -18,10 +18,13 @@ const authorDefault = "Oprah Winfrey";
     return (
       <div className="page">
         <h1 className="title">Random Quote App</h1>
-        <div className="quote-box" id="quote-box">
-          <div className="quote-text"><FaQuoteLeft/> {this.state.quote}</div>
-          <div className="quote-author">-{this.state.author}</div>
-          <button className="new-quote">New quote</button>
+        <div id="quote-box">
+          <div id="text"><FaQuoteLeft /> {this.state.quote}</div>
+          <div id="author">-{this.state.author}</div>
+              <a id="tweet-quote"  href="https://twitter.com/intent/tweet" title="Tweet this quote!" target="_blank">
+                <FaTwitter />
+              </a>
+            <button id="new-quote">New quote</button>
         </div>
       </div>
     );
