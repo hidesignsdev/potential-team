f5dd92168f9cb4e30963b394530c0a9a1ad99079
c2cdev006 <c2c.dev.006@gmail.com>
2020-03-18 10:07:30 +0700

    calculate basic
    check -- and replace to +
    can calculate + - x / .
    reset and backspace

--- javascript-calculator/src/App.js
+++ javascript-calculator/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
 export default class App extends Component {
   constructor(props) {
@@ -10,52 +10,73 @@ export default class App extends Component {
       valueTyping: ""
     }
   }
-  add = () =>{
-    // this.props.dispatch({type: 'ADD'})
-  }
-  subtract = () =>{
-    // this.props.dispatch({type: 'SUB'})
-  } 
-  clear = () =>{
-    // this.props.dispatch({type: 'CLEAR'})
-  }
-  divide = () =>{
-    // this.props.dispatch({type: 'DIV'})
-  }
-  multiply = () =>{
-    // this.props.dispatch({type: 'MUL'})
-  }
-  equal = ()=>{
-    // this.props.dispatch({type: 'EQUAL'})
-  }
-  enterNumber = (event)=>{
+  calculate(inputDisplay) {
     const {valueTyping} = this.state;
-    this.setState({valueTyping: valueTyping+event.target.value})
+    if (valueTyping.includes('--')) {
+      inputDisplay = valueTyping.replace('--', '+');
+    }
+    let arrayOperation = ['+', '-', 'x', '/', '.'];
+    while (arrayOperation.includes(inputDisplay.slice(-1))) {
+      inputDisplay = inputDisplay.slice(0, -1);
+    }
+    this.setState({ result: eval(inputDisplay) || "" + "" })
+  }
+  reset(){
+    this.setState({ valueTyping: "", result: 0 });
+  }
+  backspace(){
+    const {valueTyping} = this.state;
+    this.setState({ valueTyping: valueTyping.slice(0, -1) })
+  }
+  enterValue = (event) => {
+    const { valueTyping, result } = this.state;
+    if (result != 0) {
+      this.reset();
+    }
+
+    let valueButton = event.target.value;
+    
+    if (valueButton == "=") {
+      this.calculate(valueTyping);
+    }
+    else if (valueButton == "C") { //backspace
+      this.backspace();
+    }
+    else if (valueButton == "AC") { //reset
+      this.reset();
+    }
+    else {
+      this.setState({valueTyping: valueTyping + valueButton});
+    }
+    
   }
   render() {
+    const {valueTyping, result} = this.state;
     return (
       <div className="page">
         <div className="calculator">
-    <div className="formulaScreen">{this.state.valueTyping}</div>
-          <div id="display">{this.state.result}</div>
-          <button id="clear" value ="AC" className="standardItem" onClick={this.clear}>AC</button>
-          <button id="divide" value="/" className="operation standardItem" onClick={this.divide}>/</button>
-          <button id="multiply" value="x" className="operation standardItem" onClick={this.multiply}>X</button>
-          
-          <button id="seven" value="7" className="number standardItem" onClick={this.enterNumber}>7</button>
-          <button id="eight" value="8" className="number standardItem" onClick={this.enterNumber}>8</button>
-          <button id="nine" value="9" className="number standardItem" onClick={this.enterNumber}>9</button>
-          <button id="subtract" value="-" className="operation standardItem" onClick={this.subtract}>-</button>
-          <button id="four" value="4" className="number standardItem" onClick={this.enterNumber}>4</button>
-          <button id="five" value="5" className="number standardItem" onClick={this.enterNumber}>5</button>
-          <button id="six" value="6" className="number standardItem" onClick={this.enterNumber}>6</button>
-          <button id="add" value="+" className="operation standardItem" onClick={this.add}>+</button>
-          <button id="one" value="1" className="number standardItem" onClick={this.enterNumber}>1</button>
-          <button id="two" value="2" className="number standardItem" onClick={this.enterNumber}>2</button>
-          <button id="three" value="3" className="number standardItem" onClick={this.enterNumber}>3</button>
-          <button id="equals" value="=" className="standardItem" onClick={this.equal}>=</button>
-          <button id="zero" value="0" className="number standardItem" style={{width: "162px"}} onClick={this.enterNumber}>0</button>
-          <button id="decimal" value="." className="number standardItem" onClick={this.enterNumber}>.</button>
+          <div className="formulaScreen">{valueTyping}</div>
+          <div id="display">{result}</div>
+          <button id="allclear" value="AC" className="standardItem" onClick={this.enterValue}>AC</button>
+          <button id="clear" value="C" className="standardItem" onClick={this.enterValue}>C</button>
+          <button id="divide" value="/" className="operation standardItem" onClick={this.enterValue}>/</button>
+
+          <button id="seven" value="7" className="number standardItem" onClick={this.enterValue}>7</button>
+          <button id="eight" value="8" className="number standardItem" onClick={this.enterValue}>8</button>
+          <button id="nine" value="9" className="number standardItem" onClick={this.enterValue}>9</button>
+          <button id="multiply" value="x" className="operation standardItem" onClick={this.enterValue}>X</button>
+
+          <button id="four" value="4" className="number standardItem" onClick={this.enterValue}>4</button>
+          <button id="five" value="5" className="number standardItem" onClick={this.enterValue}>5</button>
+          <button id="six" value="6" className="number standardItem" onClick={this.enterValue}>6</button>
+          <button id="subtract" value="-" className="operation standardItem" onClick={this.enterValue}>-</button>
+          <button id="one" value="1" className="number standardItem" onClick={this.enterValue}>1</button>
+          <button id="two" value="2" className="number standardItem" onClick={this.enterValue}>2</button>
+          <button id="three" value="3" className="number standardItem" onClick={this.enterValue}>3</button>
+          <button id="add" value="+" className="operation standardItem" onClick={this.enterValue}>+</button>
+          <button id="zero" value="0" className="number standardItem" style={{ width: "162px" }} onClick={this.enterValue}>0</button>
+          <button id="decimal" value="." className="number standardItem" onClick={this.enterValue}>.</button>
+          <button id="equals" value="=" className="standardItem" onClick={this.enterValue}>=</button>
         </div>
       </div>
     );
