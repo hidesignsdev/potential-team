7a89d256ac467038b89d2e028f4418a051eb132e
Ho Thai Hoang Nam <c2c.dev.002@gmail.com>
2020-03-23 09:46:58 +0700

    refactor code

--- javascript-calculator/src/components/App.js
+++ javascript-calculator/src/components/App.js
@@ -11,68 +11,47 @@ import { numberInput, operatorInput, decimalInput, calculate, clear } from '../a
 
 class App extends Component {
 
-  handleNumberInput = (e) => {
-    this.props.dispatch(numberInput(e.target.dataset.value))
-    //this.props.onNumberInput(e.target.dataset.value);
-  };
-  handleOperatorInput = (e) => {
-    this.props.dispatch(operatorInput(e.target.dataset.value))
-    //this.props.onOperatorInput(e.target.dataset.value);
-  };
-  handleDecimalInput = (e) => {
-    this.props.dispatch(decimalInput(e.target.dataset.value))
-    //this.props.onOperatorInput(e.target.dataset.value);
-  };
-  handleCalculate = () => {
-    this.props.dispatch(calculate())
-    //this.props.onCalculator();
-  };
-  handleClear = () => {
-    this.props.dispatch(clear())
-    //this.props.onClear();
-  };
-
   render() {
     return (
       <div className="page">
         <div className="calculator">
           <div className="formulaScreen">{this.props.calculator.equation}</div>
           <div className="outputScreen" id="display">{this.props.calculator.display}</div>
-          <button id="clear" data-value="AC" onClick={this.handleClear}>AC</button>
-          <button id="divide" data-value="/" className="operation" onClick={this.handleOperatorInput}>/</button>
-          <button id="seven" data-value="7" className="number" onClick={this.handleNumberInput}>7</button>
-          <button id="eight" data-value="8" className="number" onClick={this.handleNumberInput}>8</button>
-          <button id="nine" data-value="9" className="number" onClick={this.handleNumberInput}>9</button>
-          <button id="multiply" data-value="*" className="operation" onClick={this.handleOperatorInput}>X</button>
-          <button id="four" data-value="4" className="number" onClick={this.handleNumberInput}>4</button>
-          <button id="five" data-value="5" className="number" onClick={this.handleNumberInput}>5</button>
-          <button id="six" data-value="6" className="number" onClick={this.handleNumberInput}>6</button>
-          <button id="subtract" data-value="-" className="operation" onClick={this.handleOperatorInput}>-</button>
-          <button id="one" data-value="1" className="number" onClick={this.handleNumberInput}>1</button>
-          <button id="two" data-value="2" className="number" onClick={this.handleNumberInput}>2</button>
-          <button id="three" data-value="3" className="number" onClick={this.handleNumberInput}>3</button>
-          <button id="add" data-value="+" className="operation" onClick={this.handleOperatorInput}>+</button>
-          <button id="zero" data-value="0" className="number" onClick={this.handleNumberInput}>0</button>
-          <button id="decimal" data-value="." className="number" onClick={this.handleDecimalInput}>.</button>
-          <button id="equals" data-value="=" className="operation" onClick={this.handleCalculate}>=</button>
+          <button id="clear" data-value="AC" onClick={this.props.onClear}>AC</button>
+          <button id="divide" data-value="/" className="operation" onClick={this.props.onOperatorInput}>/</button>
+          <button id="seven" data-value="7" className="number" onClick={this.props.onNumberInput}>7</button>
+          <button id="eight" data-value="8" className="number" onClick={this.props.onNumberInput}>8</button>
+          <button id="nine" data-value="9" className="number" onClick={this.props.onNumberInput}>9</button>
+          <button id="multiply" data-value="*" className="operation" onClick={this.props.onOperatorInput}>X</button>
+          <button id="four" data-value="4" className="number" onClick={this.props.onNumberInput}>4</button>
+          <button id="five" data-value="5" className="number" onClick={this.props.onNumberInput}>5</button>
+          <button id="six" data-value="6" className="number" onClick={this.props.onNumberInput}>6</button>
+          <button id="subtract" data-value="-" className="operation" onClick={this.props.onOperatorInput}>-</button>
+          <button id="one" data-value="1" className="number" onClick={this.props.onNumberInput}>1</button>
+          <button id="two" data-value="2" className="number" onClick={this.props.onNumberInput}>2</button>
+          <button id="three" data-value="3" className="number" onClick={this.props.onNumberInput}>3</button>
+          <button id="add" data-value="+" className="operation" onClick={this.props.onOperatorInput}>+</button>
+          <button id="zero" data-value="0" className="number" onClick={this.props.onNumberInput}>0</button>
+          <button id="decimal" data-value="." className="number" onClick={this.props.onDecimalInput}>.</button>
+          <button id="equals" data-value="=" className="operation" onClick={this.props.onCalculate}>=</button>
         </div>
       </div>
     );
   }
 }
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     onNumberInput: (number) => dispatch(numberInput(number)),
-//     onOperatorInput: (operator) => dispatch(operatorInput(operator)),
-//     onDecimalInput: (decimal) => dispatch(decimalInput(decimal)),
-//     onCalculator: () => dispatch(calculate()),
-//     onClear: () => dispatch(clear())
-//   }
-// }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onNumberInput: (e) => dispatch(numberInput(e.target.dataset.value)),
+    onOperatorInput: (e) => dispatch(operatorInput(e.target.dataset.value)),  
+    onDecimalInput: (e) => dispatch(decimalInput(e.target.dataset.value)),
+    onCalculate: () => dispatch(calculate()),
+    onClear: () => dispatch(clear())
+  }
+}
 const mapStateToProps = (state) => {
   return {
     calculator: state.calculateReducer
   }
 }
 
-export default connect(mapStateToProps)(App)
+export default connect(mapStateToProps,mapDispatchToProps)(App)
--- javascript-calculator/src/reducers/calculateReducer.js
+++ javascript-calculator/src/reducers/calculateReducer.js
@@ -5,122 +5,90 @@ const initState = {
     display: "0",
     equation: "",
 };
-
 const calculateReducer = (prevState = initState, action) => {
     console.log(prevState)
+    let { display, equation } = prevState
     switch (action.type) {
         case NUMBER_INPUT:
             if (prevState.display === "0" || prevState.equation.includes("=")) {
-                return {
-                    display: action.payload,
-                    equation: action.payload
-                };
+                display = action.payload
+                equation = action.payload
             } else if (prevState.equation.match(/[+\-*/]$/)) {
                 const value = prevState.equation + action.payload;
-                return {
-                    display: action.payload,
-                    equation: value
-                };
+                display = action.payload
+                equation = value
             } else if (prevState.equation.match(/[0-9.]$/)) {
                 if (prevState.equation.match(/[+\-*/]/) == null && !prevState.equation.includes("=")) {
                     const value = prevState.equation + action.payload;
-                    return {
-                        display: value,
-                        equation: value
-                    };
+                    display = value
+                    equation = value
                 } else {
-                    return {
-                        display: prevState.display + action.payload,
-                        equation: prevState.equation + action.payload
-                    };
+                    display = prevState.display + action.payload
+                    equation = prevState.equation + action.payload
                 }
             };
-            break;
+            return { display, equation }
         case OPERATOR_INPUT:
             if (prevState.equation.includes("=")) {
                 let value = prevState.display;
                 value += action.payload;
-                return {
-                    equation: value
-                }
+                equation = value
             } else if (prevState.equation !== "" && prevState.equation.match(/[ *\- +/]$/) === null) {
                 let value = prevState.equation;
                 value += action.payload;
-                return {
-                    equation: value
-                }
+                equation = value
             } else if (prevState.equation.match(/[+\- */]$/) !== null) {
                 if (prevState.equation.includes("+-") === true && action.payload !== "-") {
                     let value = prevState.equation;
                     value = value.substring(0, value.length - 2)
                     value += action.payload;
-                    return {
-                        equation: value
-                    }
+                    equation = value
                 }
                 else if (prevState.equation.includes("/-") === true && action.payload !== "-") {
                     let value = prevState.equation;
                     value = value.substring(0, value.length - 2)
                     value += action.payload
-                    return {
-                        equation: value
-                    }
+                    equation = value
                 } else if (prevState.equation.includes("*-") === true && action.payload !== "-") {
                     let value = prevState.equation;
                     value = value.substring(0, value.length - 2)
                     value += action.payload
-                    return {
-                        equation: value
-                    }
+                    equation = value
                 }
                 else if (prevState.equation.match(/[+/ *]$/) !== null && action.payload === "-") {
                     let value = prevState.equation + action.payload
-                    return {
-                        equation: value
-                    }
+                    equation = value
                 } else if (prevState.equation.match(/[+]/) !== null && action.payload !== "-") {
                     let value = prevState.equation;
                     value = value.substring(0, value.length - 1)
                     value += action.payload
-                    return {
-                        equation: value
-                    }
+                    equation = value
                 } else if (prevState.equation.match(/-/) !== null && action.payload !== "-") {
                     let value = prevState.equation;
                     value = value.substring(0, value.length - 1)
                     value += action.payload
-                    return {
-                        equation: value
-                    }
+                    equation = value
                 }
             }
-            return prevState;
+            return { prevState, display, equation }
         case DECIMAL_INPUT:
             if (prevState.equation === "" || prevState.equation.includes("=")) {
                 let value = "0."
-                return {
-                    equation: value,
-                    display: value
-                }
+                equation = value
+                display = value
             } else if (prevState.equation.match(/[+\- */]$/)) {
                 let value = "0."
-                return {
-                    display: value,
-                    equation: prevState.equation + value
-                }
+                display = value
+                equation = prevState.equation + value
             } else if (!prevState.display.includes(".")) {
-                return {
-                    equation: prevState.equation + action.payload,
-                    display: prevState.display + action.payload
-                }
+                equation = prevState.equation + action.payload
+                display = prevState.display + action.payload
             }
-            else {
-                return prevState;
-            }
+            return { prevState, display, equation }
         case CALCULATE:
             if (prevState.equation.includes("=")) {
                 const value = `${prevState.display} = ${prevState.display}`;
-                return { equation: value };
+                equation = value
             } else if (prevState.equation !== ""
                 && prevState.equation.match(/[+\-*/]/) != null
                 && prevState.equation.match(/[+\-*/]$/) == null) {
