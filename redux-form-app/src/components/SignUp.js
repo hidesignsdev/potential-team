6cf72122ba4238133ba7c13990beafc8333050aa
c2cdev006 <c2c.dev.006@gmail.com>
2020-04-08 16:27:33 +0700

    sign-up success
    install lodash

--- redux-form-app/src/components/SignUp.js
+++ redux-form-app/src/components/SignUp.js
@@ -2,11 +2,34 @@ import React from "react";
 import SignupForm from '../form/SignupForm'
 import { withRouter } from "react-router-dom";
 import {validateSignup} from "../form/validate";
+import _ from "lodash";
+
+async function submitSignUp(data) {
+    try {
+        let response = await fetch('https://api.korec-dev.scrum-dev.com/api/functions/userSignup', {
+            method: 'POST',
+            headers: {
+                'X-Parse-Application-Id': 'U2fZ7KvIHVvH4snHbkj02uKBpISSWF8C1oePV7iraoy69JrMBvPi',
+                'X-Parse-REST-API-Key': 'UrEeTwu2B5izB28HmtcOm7JpLmDSbSpxILDJ7NdXlA9InpenPj',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        let result = response.json();
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 class SignUp extends React.Component {
+    
     submit = values => {
         const {history} = this.props;
-        console.log("sign-up",values)
+        // remove cfPassword
+        const datapost = _.pick(values, ['firstName', 'lastName','email','password']);
+        const result = submitSignUp(datapost);
+        console.log("sign-up",result)
 
         history.push("/personal-info");
     }
--- redux-form-app/src/form/SignupForm.js
+++ redux-form-app/src/form/SignupForm.js
@@ -4,31 +4,11 @@ import Image from '../image/iconReact.png'
 import InputField from "./InputField";
 import { required } from "./validate";
 
-async function submitSignUp(data) {
-    try {
-        let response = await fetch('https://api.korec-dev.scrum-dev.com/api/functions/userSignup', {
-            method: 'POST',
-            headers: {
-                'X-Parse-Application-Id': 'U2fZ7KvIHVvH4snHbkj02uKBpISSWF8C1oePV7iraoy69JrMBvPi',
-                'X-Parse-REST-API-Key': 'UrEeTwu2B5izB28HmtcOm7JpLmDSbSpxILDJ7NdXlA9InpenPj',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
-        let result = response.json();
-        console.log(result);
-        return result;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-
 
 const SignupForm = props => {
     const { handleSubmit } = props
     return (
-        <form onSubmit={handleSubmit(submitSignUp)}>
+        <form onSubmit={handleSubmit}>
             <div className="form-field">
                 <img className="square" src={Image} alt=""></img>
                 <label htmlFor="firstName">First Name</label>
@@ -40,7 +20,7 @@ const SignupForm = props => {
                 <label htmlFor="password">Password</label>
                 <Field name="password" component={InputField} type="password" />
                 <label htmlFor="cfPassword">Confirm your password</label>
-                {/* <Field name="cfPassword" component={InputField} type="password" /> */}
+                <Field name="cfPassword" component={InputField} type="password" />
                 <button type="submit" className="btn btn-primary">Sign Up</button>
             </div>
             <div>
