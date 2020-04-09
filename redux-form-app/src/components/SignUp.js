4b82df8a178054d30f3a7de142b7203c4dd36754
c2cdev006 <c2c.dev.006@gmail.com>
2020-04-09 07:51:04 +0700

    post data by api to sign up
    install axios

--- redux-form-app/src/components/SignUp.js
+++ redux-form-app/src/components/SignUp.js
@@ -1,42 +1,46 @@
 import React from "react";
+import axios from "axios";
 import SignupForm from '../form/SignupForm'
 import { withRouter } from "react-router-dom";
-import {validateSignup} from "../form/validate";
+import { validateSignup } from "../form/validate";
 import _ from "lodash";
 
-async function submitSignUp(data) {
-    try {
-        let response = await fetch('https://api.korec-dev.scrum-dev.com/api/functions/userSignup', {
-            method: 'POST',
-            headers: {
-                'X-Parse-Application-Id': 'U2fZ7KvIHVvH4snHbkj02uKBpISSWF8C1oePV7iraoy69JrMBvPi',
-                'X-Parse-REST-API-Key': 'UrEeTwu2B5izB28HmtcOm7JpLmDSbSpxILDJ7NdXlA9InpenPj',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
-        let result = response.json();
-        return result;
-    } catch (error) {
-        console.error(error);
-    }
+async function submitSignUp(data, callback) {
+    await axios.post('https://api.korec-dev.scrum-dev.com/api/functions/userSignup', data, {
+        headers: {
+            'X-Parse-Application-Id': 'U2fZ7KvIHVvH4snHbkj02uKBpISSWF8C1oePV7iraoy69JrMBvPi',
+            'X-Parse-REST-API-Key': 'UrEeTwu2B5izB28HmtcOm7JpLmDSbSpxILDJ7NdXlA9InpenPj',
+            'Content-Type': 'application/json',
+        },
+    }).then(response => {
+        console.log(response)
+        callback && callback(true, response)
+    }).catch(error => {
+        console.error(error)
+        callback && callback(false, error)
+    });
 }
 
 class SignUp extends React.Component {
-    
-    submit = values => {
-        const {history} = this.props;
-        // remove cfPassword
-        const datapost = _.pick(values, ['firstName', 'lastName','email','password']);
-        const result = submitSignUp(datapost);
-        console.log("sign-up",result)
 
-        history.push("/personal-info");
+    submit = values => {
+        const { history } = this.props;
+        // remove cfPassword
+        const datapost = _.pick(values, ['firstName', 'lastName', 'email', 'password']);
+        submitSignUp(datapost, (success, response) => {
+            if(success){
+                history.push("/personal-info");
+            } else {
+                console.log(response);
+                // alert(_.get(response, "error"))
+                alert("Vui lòng thử lại!")
+            }
+        });
     }
     render() {
         return (
             <div className="container">
-                <SignupForm onSubmit={this.submit} validate={validateSignup}/>
+                <SignupForm onSubmit={this.submit} validate={validateSignup} />
             </div>
         );
     }
